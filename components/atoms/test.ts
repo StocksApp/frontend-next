42995109dcf91e0620fc1358a2c4e02b7ca42607
server-of-disaster <bartoszkaszuba1@gmail.com>
2021-10-26 22:51:58 +0200

    Bunch of prettier fixes

--- components/atoms/test.ts
+++ components/atoms/test.ts
@@ -1,10 +1,10 @@
-export const APP_VERSION = 'v1.0.0'
+export const APP_VERSION = 'v1.0.0';
 
 export const object = {
   whatever: '1',
   hejka: '2',
-}
+};
 
-const valueGetter = (value: number) => value
+const valueGetter = (value: number) => value;
 
-const value = valueGetter(parseInt(object.hejka, 10))
+const value = valueGetter(parseInt(object.hejka, 10));
